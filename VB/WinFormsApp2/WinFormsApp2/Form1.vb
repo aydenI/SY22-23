8e65f91d04ab8c66c96b2b9f41618ffc3186229b
monkeyz <aiivy@learner.dvusd.org>
2022-08-17 14:10:11 -0700

    wednesday

---
+++ VB/WinFormsApp2/WinFormsApp2/Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form1
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Me.BackgroundImage = PictureBox1.Image
+
+    End Sub
+
+    Private Sub PictureBox3 Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Me.BackgroundImage = PictureBox2.Image
+
+End Class
